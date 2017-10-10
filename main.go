c84dab278243661889906516d0206cc18de305dc
Thorsten Klein <thorsten.klein.95@googlemail.com>
2017-10-10 14:36:03 +0200

    - removed transport as service shares metrics passively (on scrape)

--- main.go
+++ main.go
@@ -6,13 +6,12 @@ import (
 	"os"
 
 	// Prometheus for logging/metrics
-	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	// go-kit
 	"github.com/go-kit/kit/log"
 	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
-	//httptransport "github.com/go-kit/kit/transport/http"
 )
 
 func main() {
@@ -20,19 +19,19 @@ func main() {
 
 	// Prometheus metrics settings for this service
 	fieldKeys := []string{"method", "error"} // label names
-	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+	requestCount := kitprometheus.NewCounterFrom(prometheus.CounterOpts{
 		Namespace: "EDGECAST_SERVICE",
 		Subsystem: "service_metrics",
 		Name:      "request_count",
 		Help:      "Number of requests received.",
 	}, fieldKeys)
-	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+	requestLatency := kitprometheus.NewSummaryFrom(prometheus.SummaryOpts{
 		Namespace: "EDGECAST_SERVICE",
 		Subsystem: "service_metrics",
 		Name:      "request_latency_microseconds",
 		Help:      "Total duration of requests in microseconds.",
 	}, fieldKeys)
-	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+	countResult := kitprometheus.NewSummaryFrom(prometheus.SummaryOpts{
 		Namespace: "EDGECAST_SERVICE",
 		Subsystem: "service_metrics",
 		Name:      "count_result",
@@ -45,15 +44,7 @@ func main() {
 	svc = loggingMiddleware{logger, svc} // attach logger to service
 	svc = instrumentingMiddleware{requestCount, requestLatency, countResult, svc}
 
-	// initiate server with service + endpoint
-	/*getDataHandler := httptransport.NewServer(
-		makeGetDataEndpoint(svc),
-		nil,
-		encodeResponse,
-	)*/
-
 	// connect handlers
-	//http.Handle("/service", )
 	http.Handle("/metrics", promhttp.Handler())
 
 	// set up logger and start service on port 8090
