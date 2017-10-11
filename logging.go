b46f2f9b9a46eef6323ebde18ada3b72b8d9a4be
Thorsten Klein <thorsten.klein.95@googlemail.com>
2017-10-11 12:49:52 +0200

    - logging middleware now implements EdgecastInterface (edgecast client)

--- logging.go
+++ logging.go
@@ -29,3 +29,48 @@ func (mw loggingMiddleware) Bandwidth(platform int) (bandwidthData *ec.Bandwidth
 	bandwidthData, err = mw.next.Bandwidth(platform) // hand function call to service
 	return
 }
+
+func (mw loggingMiddleware) Connections(platform int) (connectionData *ec.ConnectionData, err error) {
+	defer func(begin time.Time) {
+		_ = mw.logger.Log( // params: alternating key-value-key-value-...
+			"method", "Bandwidth",
+			"input", platform,
+			"output", connectionData,
+			"err", err,
+			"took", time.Since(begin),
+		)
+	}(time.Now())
+
+	connectionData, err = mw.next.Connections(platform) // hand function call to service
+	return
+}
+
+func (mw loggingMiddleware) CacheStatus(platform int) (cacheStatusData *ec.CacheStatusData, err error) {
+	defer func(begin time.Time) {
+		_ = mw.logger.Log( // params: alternating key-value-key-value-...
+			"method", "Bandwidth",
+			"input", platform,
+			"output", cacheStatusData,
+			"err", err,
+			"took", time.Since(begin),
+		)
+	}(time.Now())
+
+	cacheStatusData, err = mw.next.CacheStatus(platform) // hand function call to service
+	return
+}
+
+func (mw loggingMiddleware) StatusCodes(platform int) (statusCodeData *ec.StatusCodeData, err error) {
+	defer func(begin time.Time) {
+		_ = mw.logger.Log( // params: alternating key-value-key-value-...
+			"method", "Bandwidth",
+			"input", platform,
+			"output", statusCodeData,
+			"err", err,
+			"took", time.Since(begin),
+		)
+	}(time.Now())
+
+	statusCodeData, err = mw.next.StatusCodes(platform) // hand function call to service
+	return
+}
